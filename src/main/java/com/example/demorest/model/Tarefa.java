1f72608ed20c2ee088f1d3b979b90ee1cb09ab07
JF Gomes <jfgomes@JFs-MacBook-Air.local>
2025-08-22 19:32:44 -0400

    first commit

---
+++ src/main/java/com/example/demorest/model/Tarefa.java
@@ -0,0 +1,38 @@
+package com.example.demorest.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+
+public class Tarefa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String nome;
+	private String data;
+	private String responsavel;
+	
+	// Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getData() { return data; }
+    public void setData(String data) { this.data = data; }
+
+    public String getResponsavel() { return responsavel; }
+    public void setResponsavel(String responsavel) { this.responsavel = responsavel; }
+	
+}
