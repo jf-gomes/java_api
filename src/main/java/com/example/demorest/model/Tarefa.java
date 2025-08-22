6d02ce56059128797bf8fdf6462ab053682ec7e4
JF Gomes <jfgomes@JFs-MacBook-Air.local>
2025-08-22 19:38:07 -0400

    removed lombok

--- src/main/java/com/example/demorest/model/Tarefa.java
+++ src/main/java/com/example/demorest/model/Tarefa.java
@@ -3,13 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 
 public class Tarefa {
